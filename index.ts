423aab43d8d629e0171e02980d66109b47c570c5
Idan Heskel <idan7856@gmail.com>
2025-06-04 15:13:53 +0300

    general changes

--- index.ts
+++ index.ts
@@ -12,6 +12,7 @@ const port = 3000;
 
 let token: string | undefined = undefined;
 let tokenExpiresOn: number | undefined = undefined;
+let timeSinceLastTokenFetchAttempt: number | undefined = undefined
 
 proxy.listen(port, async () => {
   await getToken();
@@ -32,6 +33,8 @@ const getToken = async (): Promise<string | undefined> => {
   if (token && tokenExpiresOn && now < tokenExpiresOn - 60 * 1000) {
     return token;
   } else {
+    // if an attempt to fetch the token was made any time in the last 30 seconds, don't send
+    if (timeSinceLastTokenFetchAttempt && now - timeSinceLastTokenFetchAttempt < 30 * 1000) return token;
     const result = await clientApp.acquireTokenByDeviceCode({
       scopes: [`${process.env.SHAREPOINT_URL}/.default`],
       deviceCodeCallback: (response) => {
@@ -39,6 +42,7 @@ const getToken = async (): Promise<string | undefined> => {
       },
     });
     token = result?.accessToken;
+    timeSinceLastTokenFetchAttempt = now;
     tokenExpiresOn = result?.expiresOn ? new Date(result.expiresOn).getTime() : undefined;
     return token;
   }
@@ -46,7 +50,7 @@ const getToken = async (): Promise<string | undefined> => {
 
 proxy.all("/*all", async (req: Request, res: Response) => {
   const token = await getToken();
-  if (!token) return void res.status(500).send("Token fetch failed.");
+  if (!token) return void res.status(401).send("Token fetch failed. Try to refresh Proxy");
 
   const sharePointUrl = `${process.env.SHAREPOINT_URL}${req.originalUrl}`;
   console.log("request initiated with endpoint: " + sharePointUrl);
@@ -56,18 +60,20 @@ proxy.all("/*all", async (req: Request, res: Response) => {
     Object.keys(req.body).length > 0;
   try {
     const spResponse = await fetch(sharePointUrl, {
-      ...req,
       method: req.method,
       headers: {
-        Authorization: `Bearer ${token}`,
-        Accept: "application/json;odata=verbose",
         ...(req.headers as { [key: string]: string }),
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json;odata=verbose',
       },
       body: hasBody ? JSON.stringify(req.body) : undefined,
     });
     
-    const data = await spResponse.json();
-    res.status(spResponse.status).send(data);
+    res.status(spResponse.status);
+
+    spResponse.headers.get('Content-Type')?.includes('application/json')
+      ? res.send(await spResponse.json())
+      : res.send(await spResponse.text());
   } catch (error) {
     console.error("Proxy error:", error);
     res.status(500).send("Proxy request failed");
